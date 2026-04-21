0e1be96a54e554dcf0530ccea627b83ac249de15
rolfDSP <rolfesslinger@web.de>
2026-04-21 05:21:43 -0400

    Expanded example with exercise

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,12 @@ function calculateArea() {
  let area = length * width;
   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculateTotalAmount() {
+    amount1 = parseFloat(document.getElementById('amount1').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
+
+    let totalAmount = amount1 + amount2 + amount3;
+    document.getElementById('total_amount').innerText = `Total amount is: ${totalAmount}`;
+}
